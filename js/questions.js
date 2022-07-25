83f197373ecb9309157164c281dd5e4ddab3f6f1
João Renato Vaz <joaorenatovaz@gmail.com>
2022-07-25 09:30:55 -0300

    Criando uma array para as questões

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        question: "O que segnifica HTML ?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Qual o significado de CSS?",
+        answer: "Cascading Style Sheet",
+        options: [
+          "Common Style Sheet",
+          "Colorful Style Sheet",
+          "Computer Style Sheet",
+          "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Qual o significado de PHP?",
+        answer: "Hypertext Preprocessor",
+        options: [
+          "Hypertext Preprocessor",
+          "Hypertext Programming",
+          "Hypertext Preprogramming",
+          "Hometext Preprocessor"
+        ]
+      },
+      {
+        numb: 4,
+        question: "Qual o significado de SQL",
+        answer: "Structured Query Language",
+        options: [
+          "Stylish Question Language",
+          "Stylesheet Query Language",
+          "Statement Question Language",
+          "Structured Query Language"
+        ]
+      },
+      {
+        numb: 5,
+        question: "Qual o significado de XML?",
+        answer: "eXtensible Markup Language",
+        options: [
+          "eXtensible Markup Language",
+          "eXecutable Multiple Language",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+
+];
